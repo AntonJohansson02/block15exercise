529ef4276677f142b7877d06cd48c68f7b0f4ab1
niklasbroberg <niklas.broberg@gmail.com>
2024-03-04 08:14:48 +0100

    Update numbering for exercises, add new StatefulTransform exercise.

---
+++ src/edu/chalmers_gu_cse/oopd/exercises/polygonModel/macro/StatefulTransform.java
@@ -0,0 +1,14 @@
+package edu.chalmers_gu_cse.oopd.exercises.polygonModel.macro;
+
+import edu.chalmers_gu_cse.oopd.exercises.polygonModel.polygon.Polygon;
+
+public class StatefulTransform implements Transform {
+
+    // TODO 6: Implement a transform that can change state depending
+    //  on the clock OR through user interaction. What design patterns
+    //  will help you?
+    @Override
+    public Polygon transform(Polygon p) {
+        return null;
+    }
+}
--- src/edu/chalmers_gu_cse/oopd/exercises/polygonModel/polygon/internal/PolygonVisitor.java
+++ src/edu/chalmers_gu_cse/oopd/exercises/polygonModel/polygon/internal/PolygonVisitor.java
@@ -2,7 +2,7 @@ package edu.chalmers_gu_cse.oopd.exercises.polygonModel.polygon.internal;
 
 public interface PolygonVisitor<R> {
 
-    // TODO 5: Visitor Pattern: Make all (components of) polygons visitable,
+    // TODO 4a: Visitor Pattern: Make all (components of) polygons visitable,
     //  and define here the components that a visitor, via pattern matching,
     //  needs to cover. Let R be the result type of the operation that the
     //  visitor represents.
